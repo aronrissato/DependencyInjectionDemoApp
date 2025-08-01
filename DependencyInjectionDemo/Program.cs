110401be97b83805a35005ae81d6c7d424f63f52
Aron Rissato <aronhrissato@gmail.com>
2025-08-01 13:45:30 -0400

    Adding Serilog.AspNetCore

--- DependencyInjectionDemo/Program.cs
+++ DependencyInjectionDemo/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddTransient<IDemoLogic, BetterDemoLogic>();
+builder.Services.AddTransient<IDemoLogic, DemoLogic>();
 
 var app = builder.Build();
 
